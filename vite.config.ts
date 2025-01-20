095090bb1f88658c0fba984091e3f2928d5f2110
Yaniel <yaniel.marron@gmail.com>
2025-01-20 19:52:12 -0300

    Publish changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,28 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name;
+          if (!name) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          let extType = name.split('.').at(1);
+          if (extType && /png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
   },
+  publicDir: 'public',
 });
